709c2732d9413ccaaaf0a5577dc88879843137f1
unknown <kadi.marss@voco.ee>
2022-01-31 15:31:33 +0200

    chat first steps

---
+++ src/components/Chat.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import '../App.css';
+function Chat() {
+    return (
+        <div className="Chat">
+            <h3>KHK</h3>
+
+            <div className="chat__container">
+
+
+            </div>
+        </div>
+    );
+}
+export default Chat;
