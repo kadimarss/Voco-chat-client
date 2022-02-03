8f4f77a0a89ed0387583162d1fd6f113f67b14c3
Dell <kadi.marss@voco.ee>
2022-02-03 12:07:17 +0200

    new card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Chat from "./components/Chat";
-
+import Card from "./components/Card";
 function App() {
   return (
     <div>
       <Chat/>
+        <Card/>
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import '../App.css';
+function Card() {
+    return (
+        <div className="Card">
+            <h3>KHK</h3>
+
+            <div className="card_container">
+                <div>
+                    <h3>text</h3>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+export default Card;
