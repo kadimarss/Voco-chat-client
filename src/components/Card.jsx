4855c10013279b09285228d44df57a3c7cae2b4e
Dell <kadi.marss@voco.ee>
2022-02-14 10:30:11 +0200

    Error

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import '../App.css';
+
+export interface ChatCardProps{
+    user:string;
+    text: string;
+    date:string;
+    counter:string;
+}
+
 function Card() {
     return (
         <div className="Card">
-            <h3>KHK</h3>
-
             <div className="card_container">
-                <div>
-                    <h3>text</h3>
-                </div>
+                <div className="text">{text}</div>
+                        <div className="user">{user}</div>
+                        <div className='card_date'>{date}</div>
+                        <div className="wordCounter"> {counter}</div>
 
             </div>
         </div>
     );
 }
+
 export default Card;
--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import '../App.css';
+import data from '../data.json';
+import Card from "./Card";
 function Chat() {
     return (
         <div className="Chat">
             <h3>KHK</h3>
 
-            <div className="chat__container">
-
+            <div className="chat_container">
+                {data.map(cardItem) => {
+                    return(
+                        <Card key={cardItem.id} user={cardItem.username} text={carsItem.text} date={cardItem.date} counter={cardItem.counter} />
+                    )
+                })}
 
             </div>
         </div>
